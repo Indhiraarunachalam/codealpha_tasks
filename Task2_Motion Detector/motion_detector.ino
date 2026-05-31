2cba5e54686224c7d9aa033555fa68b9cdae50d1
Indhira A <indhiraarunachalam11@gmail.com>
2026-05-31 13:37:17 +0530

    Update motion_detector.ino

--- Task2_Motion Detector/motion_detector.ino
+++ Task2_Motion Detector/motion_detector.ino
@@ -1,1 +1,26 @@
+int pirPin = 2;
+int ledPin = 13;
+int motionState = 0;
 
+void setup() {
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  motionState = digitalRead(pirPin);
+
+  if (motionState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Motion Detected!");
+  }
+
+  else {
+    digitalWrite(ledPin, LOW);
+    Serial.println("No Motion");
+  }
+
+  delay(1000);
+}
